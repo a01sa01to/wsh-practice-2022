54bb6f408cabe140577633499c420c246cc9f35d
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-13 01:00:53 +0900

    feat: `react-dom` の lazy load

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import { App } from "./foundation/App";
 
 const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+import("react-dom").then((ReactDOM) => {
+  ReactDOM.render(<App />, root);
+});
