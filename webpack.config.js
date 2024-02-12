f017e1fd5755b420f8401d35f48a761b932ea483
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-12 15:47:18 +0900

    feat: analyzer 入れる

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 
 const CopyPlugin = require("copy-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const nodeExternals = require("webpack-node-externals");
 
 function abs(...args) {
@@ -57,6 +58,9 @@ module.exports = [
       new CopyPlugin({
         patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
       }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+      })
     ],
     resolve: {
       extensions: [".js", ".jsx"],
