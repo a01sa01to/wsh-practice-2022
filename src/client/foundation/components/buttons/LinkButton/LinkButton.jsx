de4ac67e45b292e85f1c31ed83fa332c7d129d24
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-14 00:28:54 +0900

    fix: css module path

--- src/client/foundation/components/buttons/LinkButton/LinkButton.jsx
+++ src/client/foundation/components/buttons/LinkButton/LinkButton.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import React from "react"
 import { Link } from "react-router-dom";
 
-import style from "./linkButton.module.css";
+import style from "./linkbutton.module.css";
 
 export const LinkButton = (props) => {
   return <Link {...props} className={clsx(props.className, style.link)} />;
