767f3f48876fea8dca5200da966bfe3c766d594e
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-15 00:35:45 +0900

    chore: 不要になった component 削除

--- src/client/foundation/components/layouts/Container/Container.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import style from "./style.module.css";
-
-/** @type {React.FC} */
-export const Container = ({ children }) => {
-  return <div className={style.wrapper}>{children}</div>;
-};
