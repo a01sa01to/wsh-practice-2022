546ade1b78490c84f7376cb5041b84818a2c5cd8
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-15 12:00:40 +0900

    fix rewrite path

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       fallback: [
         {
           source: '/api/:path*',
-          destination: 'https://wsh2022-practice-j5kcv767ma-an.a.run.app/:path*'
+          destination: 'https://wsh2022-practice-j5kcv767ma-an.a.run.app/api/:path*'
         },
       ],
     }
