8a358b97c9775221e17a8924bed3490b2f650ac2
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-15 15:59:59 +0900

    chore: remove unused files

--- src/client/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { App } from "./foundation/App";
-
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
