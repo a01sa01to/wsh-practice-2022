aac6731756c70edf3e6bb59786b6e872da14ac68
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-15 18:48:05 +0900

    feat: spa ルートを消す

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,6 @@ import fastify from "fastify";
 import { User } from "../model/index.js";
 
 import { apiRoute } from "./routes/api.js";
-import { spaRoute } from "./routes/spa.js";
 import { createConnection } from "./typeorm/connection.js";
 import { initialize } from "./typeorm/initialize.js";
 
@@ -29,21 +28,11 @@ server.addHook("onRequest", async (req, res) => {
 });
 
 server.addHook("onRequest", async (req, res) => {
-  if (req.url.includes("initialize")) {
-    res.header("Cache-Control", "no-cache, no-store, no-transform");
-    res.header("Connection", "close");
-  } else {
-    if (req.url.includes("api") && !req.url.includes("zengin")) {
-      res.header("Cache-Control", "no-cache, no-store, no-transform");
-    } else {
-      res.header("Cache-Control", "public, max-age=31536000, immutable");
-    }
-    res.header("Connection", "keep-alive");
-  }
+  res.header("Cache-Control", "no-cache, no-store, no-transform");
+  res.header("Connection", "close");
 });
 
 server.register(apiRoute, { prefix: "/api" });
-server.register(spaRoute);
 
 const start = async () => {
   try {
